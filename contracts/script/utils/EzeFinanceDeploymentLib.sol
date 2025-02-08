cbf8761d85abd9bf148f1123e5c025c5e3b19a78
akbarwijayaa <jimsaber02@gmail.com>
2025-02-08 22:32:53 +0700

    build: deployer avs and sc

---
+++ contracts/script/utils/EzeFinanceDeploymentLib.sol
@@ -0,0 +1,150 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import {ProxyAdmin} from "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
+import {TransparentUpgradeableProxy} from
+    "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+import {Script} from "forge-std/Script.sol";
+import {console2} from "forge-std/Test.sol";
+import {Vm} from "forge-std/Vm.sol";
+import {stdJson} from "forge-std/StdJson.sol";
+import {ECDSAStakeRegistry} from "@eigenlayer-middleware/src/unaudited/ECDSAStakeRegistry.sol";
+import {EzeFinanceServiceManager} from "../../src/EzeFinanceServiceManager.sol";
+import {IDelegationManager} from "@eigenlayer/contracts/interfaces/IDelegationManager.sol";
+import {Quorum} from "@eigenlayer-middleware/src/interfaces/IECDSAStakeRegistryEventsAndErrors.sol";
+import {UpgradeableProxyLib} from "./UpgradeableProxyLib.sol";
+import {CoreDeploymentLib} from "./CoreDeploymentLib.sol";
+import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
+
+library EzeFinanceDeploymentLib {
+    using stdJson for *;
+    using Strings for *;
+    using UpgradeableProxyLib for address;
+
+    Vm internal constant vm = Vm(address(uint160(uint256(keccak256("hevm cheat code")))));
+
+    struct DeploymentData {
+        address ezeFinanceServiceManager;
+        address stakeRegistry;
+        address strategy;
+        address token;
+    }
+
+    function deployContracts(
+        address proxyAdmin,
+        CoreDeploymentLib.DeploymentData memory core,
+        Quorum memory quorum
+    ) internal returns (DeploymentData memory) {
+        DeploymentData memory result;
+
+        // First, deploy upgradeable proxy contracts that will point to the implementations.
+        result.ezeFinanceServiceManager = UpgradeableProxyLib.setUpEmptyProxy(proxyAdmin);
+        result.stakeRegistry = UpgradeableProxyLib.setUpEmptyProxy(proxyAdmin);
+        // Deploy the implementation contracts, using the proxy contracts as inputs
+        address stakeRegistryImpl =
+            address(new ECDSAStakeRegistry(IDelegationManager(core.delegationManager)));
+        address ezeFinanceServiceManagerImpl = address(
+            new EzeFinanceServiceManager(
+                core.avsDirectory, result.stakeRegistry, core.rewardsCoordinator, core.delegationManager
+            )
+        );
+        // Upgrade contracts
+        bytes memory upgradeCall = abi.encodeCall(
+            ECDSAStakeRegistry.initialize, (result.ezeFinanceServiceManager, 0, quorum)
+        );
+        UpgradeableProxyLib.upgradeAndCall(result.stakeRegistry, stakeRegistryImpl, upgradeCall);
+        UpgradeableProxyLib.upgrade(result.ezeFinanceServiceManager, ezeFinanceServiceManagerImpl);
+
+        return result;
+    }
+
+    function readDeploymentJson(
+        uint256 chainId
+    ) internal returns (DeploymentData memory) {
+        return readDeploymentJson("deployments/", chainId);
+    }
+
+    function readDeploymentJson(
+        string memory directoryPath,
+        uint256 chainId
+    ) internal returns (DeploymentData memory) {
+        string memory fileName = string.concat(directoryPath, vm.toString(chainId), ".json");
+
+        require(vm.exists(fileName), "Deployment file does not exist");
+
+        string memory json = vm.readFile(fileName);
+
+        DeploymentData memory data;
+        /// TODO: 2 Step for reading deployment json.  Read to the core and the AVS data
+        data.ezeFinanceServiceManager = json.readAddress(".contracts.ezeFinanceServiceManager");
+        data.stakeRegistry = json.readAddress(".contracts.stakeRegistry");
+        data.strategy = json.readAddress(".contracts.strategy");
+        data.token = json.readAddress(".contracts.token");
+
+        return data;
+    }
+
+    /// write to default output path
+    function writeDeploymentJson(
+        DeploymentData memory data
+    ) internal {
+        writeDeploymentJson("deployments/eze-finance/", block.chainid, data);
+    }
+
+    function writeDeploymentJson(
+        string memory outputPath,
+        uint256 chainId,
+        DeploymentData memory data
+    ) internal {
+        address proxyAdmin =
+            address(UpgradeableProxyLib.getProxyAdmin(data.ezeFinanceServiceManager));
+
+        string memory deploymentData = _generateDeploymentJson(data, proxyAdmin);
+
+        string memory fileName = string.concat(outputPath, vm.toString(chainId), ".json");
+        if (!vm.exists(outputPath)) {
+            vm.createDir(outputPath, true);
+        }
+
+        vm.writeFile(fileName, deploymentData);
+        console2.log("Deployment artifacts written to:", fileName);
+    }
+
+    function _generateDeploymentJson(
+        DeploymentData memory data,
+        address proxyAdmin
+    ) private view returns (string memory) {
+        return string.concat(
+            '{"lastUpdate":{"timestamp":"',
+            vm.toString(block.timestamp),
+            '","block_number":"',
+            vm.toString(block.number),
+            '"},"addresses":',
+            _generateContractsJson(data, proxyAdmin),
+            "}"
+        );
+    }
+
+    function _generateContractsJson(
+        DeploymentData memory data,
+        address proxyAdmin
+    ) private view returns (string memory) {
+        return string.concat(
+            '{"proxyAdmin":"',
+            proxyAdmin.toHexString(),
+            '","ezeFinanceServiceManager":"',
+            data.ezeFinanceServiceManager.toHexString(),
+            '","ezeFinanceServiceManagerImpl":"',
+            data.ezeFinanceServiceManager.getImplementation().toHexString(),
+            '","stakeRegistry":"',
+            data.stakeRegistry.toHexString(),
+            '","stakeRegistryImpl":"',
+            data.stakeRegistry.getImplementation().toHexString(),
+            '","strategy":"',
+            data.strategy.toHexString(),
+            '","token":"',
+            data.token.toHexString(),
+             '"}'
+        );
+    }
+}
